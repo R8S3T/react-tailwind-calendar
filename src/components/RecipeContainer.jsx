f5b8b02311c22f1a720e8965d6b7a42b59591111
Rebecca <r.stelter@gmx.com>
2023-07-30 15:28:37 +0200

    Add Avatar Option to settings page

--- src/components/RecipeContainer.jsx
+++ src/components/RecipeContainer.jsx
@@ -1,5 +1,3 @@
-// Title for recipe still needs to be added
-
 import React, { useState, useEffect } from 'react';
 
 const RecipeContainer = () => {
@@ -7,40 +5,62 @@ const RecipeContainer = () => {
     const [instructions, setInstructions] = useState('');
 
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        const preference = JSON.parse(localStorage.getItem('preference') || '{}');
+        let url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+        if (preference.category) {
+            url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${preference.category}`;
+        } else if (preference.area) {
+            url = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${preference.area}`;
+        } else if (preference.ingredient) {
+            url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${preference.ingredient}`;
+        }
+
+        fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            setRecipe(data.meals[0]);
-            setInstructions(data.meals[0].strInstructions);
+            if (data && data.meals && data.meals.length > 0) {
+                // Select a random meal from the returned list
+                const randomMealIndex = Math.floor(Math.random() * data.meals.length);
+                const mealId = data.meals[randomMealIndex].idMeal;
+                fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data && data.meals && data.meals.length > 0) {
+                        setRecipe(data.meals[0]);
+                        setInstructions(data.meals[0].strInstructions);
+                    }
+                });
+            }
         })
         .catch((error) => console.error('Error:', error));
     }, []);
 
+    if (!recipe) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className="h-full md:max-h-[500px] overflow-auto bg-my-grey-2 flex flex-col md:flex-row p-4">
-            {recipe && (
-                <img
-                    src={recipe.strMealThumb}
-                    className="w-full md:w-1/2 object-cover h-full md:h-auto"
-                    alt={recipe.strMeal}
-                />
-            )}
+            <img
+                src={recipe.strMealThumb}
+                className="w-full md:w-1/2 object-cover h-full md:h-auto"
+                alt={recipe.strMeal}
+            />
             <div className="ml-4 mt-4 md:mt-0 md:ml-4 w-full md:w-1/2">
-                {instructions && (
-                    <>
-                        <h2 className="md:text-lg font-bold">{recipe.strMeal}</h2>
-                        <p className="md:text-sm line-clamp-2">{instructions}</p>
-                        {recipe.strSource && (
-                            <a href={recipe.strSource} target='_blank' rel='noopener noreferrerer' className='mt-2'>
-                                <h3 className="mt-2">Full instructions...</h3>
-                            </a>
-                        )}
-                    </>
+                <h2 className="md:text-lg font-bold">{recipe.strMeal}</h2>
+                <p className="md:text-sm line-clamp-2">{instructions}</p>
+                {recipe.strSource && (
+                    <a href={recipe.strSource} target='_blank' rel='noopener noreferrerer' className='mt-2'>
+                        <h3 className="mt-2">Full instructions...</h3>
+                    </a>
                 )}
             </div>
         </div>
     );
-    
 };
 
 export default RecipeContainer;
+
+
+
--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -1,22 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const SettingsPage = () => {
     const [name, setName] = useState(localStorage.getItem('userName') || '');
+    const [email, setEmail] = useState(localStorage.getItem('userEmail') || '');
+    const [emailError, setEmailError] = useState(null);
+    const [preference, setPreference] = useState({
+        category: '',
+        area: '',
+        ingredient: '',
+    });
+
+    const categories = ['Seafood', 'Vegetarian', 'Vegan', 'Beef'];
+    const areas = ['Canadian', 'Italian', 'Mexican', 'Indian'];
+    const ingredients = ['Chicken', 'Tomatoes', 'Beef', 'Onions'];
 
     const handleNameChange = (e) => {
         setName(e.target.value);
-        localStorage.setItem('userName', e.target.value);
     }
 
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        if (e.target.value.trim() === '') {
+            setEmailError('Email is required for creating a unique avatar.');
+        } else {
+            setEmailError(null);
+        }
+    }
+
+    const saveSettings = () => {
+        if (email.trim() === '') {
+            setEmailError('Email is required for creating a unique avatar.');
+            return;
+        }
+        localStorage.setItem('userName', name);
+        localStorage.setItem('userEmail', email);
+        localStorage.setItem('preference', JSON.stringify(preference));
+    }
+
+    useEffect(() => {
+        const savedPreference = localStorage.getItem('preference');
+        if (savedPreference) {
+            setPreference(JSON.parse(savedPreference));
+        }
+    }, []);
+
+    const handleSelectChange = (e) => {
+        const updatedPreference = {...preference, [e.target.name]: e.target.value};
+        setPreference(updatedPreference);
+    };
+
     return (
-        <div>
-            <h1>Settings</h1>
-            <label>
-                Your name:
-                <input type='text' value={name} onChange={handleNameChange} />
-            </label>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 px-4 py-2">
+            <h1 className="text-3xl font-bold mb-4">Settings</h1>
+            <form className="bg-white shadow-md rounded px-12 pt-6 pb-8 mb-4 max-w-lg w-full">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-lg font-bold mb-2">
+                        Name:
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" value={name} onChange={handleNameChange} />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-lg font-bold mb-2">
+                        Email: <span className="text-red-500">*</span>
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="email" value={email} onChange={handleEmailChange} />
+                    {emailError && <p className="text-red-500 text-xs italic">{emailError}</p>}
+                    <p className="text-gray-600 text-xs italic">*This field is required to create a unique avatar.</p>
+                </div>
+                <div className="mb-4">
+                    <h2 className="block text-gray-700 text-lg font-bold mb-2">Meal Preferences</h2>
+                    <label className="block text-gray-700 text-md mb-2">
+                        Food Category:
+                    </label>
+                    <select className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="category" value={preference.category} onChange={handleSelectChange}>
+                        <option value="">--Surprise Me--</option>
+                        {categories.map(category => <option value={category}>{category}</option>)}
+                    </select>
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-md mb-2">
+                        Ingredient:
+                    </label>
+                    <select className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="ingredient" value={preference.ingredient} onChange={handleSelectChange}>
+                        <option value="">--Surprise Me--</option>
+                        {ingredients.map(ingredient => <option value={ingredient}>{ingredient}</option>)}
+                    </select>
+                </div>
+                <div className="flex items-center justify-end">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={saveSettings}>
+                        Save Settings
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
 
 export default SettingsPage;
+
+
+
+
+
+
